af7d6c518aa95aa074e52db9d7ac399f0550bf85
Darren Luo <jn296@qq.com>
2024-01-27 15:08:45 +0000

    add init sql to insert ord_network_type(mainnet)

--- dockerfiles/init-sql/main_init.sql
+++ dockerfiles/init-sql/main_init.sql
@@ -75,6 +75,8 @@ CREATE TABLE public.ord_network_type (
 	CONSTRAINT ord_network_type_pk PRIMARY KEY (id)
 );
 
+INSERT INTO ord_network_type (network_type) VALUES 'mainnet'
+
 CREATE TABLE public.ord_indexer_version (
 	id bigserial NOT NULL,
 	indexer_version text NOT NULL,
